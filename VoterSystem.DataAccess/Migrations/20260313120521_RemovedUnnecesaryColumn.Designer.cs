ac5aa09f3901584fc57511419283612766e2858e
Kmeczó <akmeczo@gmail.com>
2026-03-13 18:19:23 +0100

    asde

--- VoterSystem.DataAccess/Migrations/20260313120521_RemovedUnnecesaryColumn.Designer.cs
+++ VoterSystem.DataAccess/Migrations/20260313120521_RemovedUnnecesaryColumn.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using VoterSystem.DataAccess;
@@ -11,9 +12,11 @@ using VoterSystem.DataAccess;
 namespace VoterSystem.DataAccess.Migrations
 {
     [DbContext(typeof(VoterSystemDbContext))]
-    partial class VoterSystemDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260313120521_RemovedUnnecesaryColumn")]
+    partial class RemovedUnnecesaryColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ VoterSystem.DataAccess/Migrations/20260313120521_RemovedUnnecesaryColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VoterSystem.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedUnnecesaryColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VotingParticipationId",
+                table: "VotingParticipations");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "VotingParticipationId",
+                table: "VotingParticipations",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+    }
+}

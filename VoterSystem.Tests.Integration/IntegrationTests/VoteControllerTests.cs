b7ade97c96c5c608010416220831b408fce89d2d
andraskmeczo <akmeczo@gmail.com>
2026-03-20 10:44:43 +0100

    Fixed an issue with the group not being showed everywhere

--- VoterSystem.Tests.Integration/IntegrationTests/VoteControllerTests.cs
+++ VoterSystem.Tests.Integration/IntegrationTests/VoteControllerTests.cs
@@ -1,6 +1,4 @@
 using System.Net;
-using System.Net.Http.Headers;
-using System.Net.Http.Json;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using VoterSystem.DataAccess;
